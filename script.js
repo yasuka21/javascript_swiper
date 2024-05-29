2fd7d3993b8f82bd760dcf8e2dc865327dfbcc3e
yasuka21 <167289083+yasuka21@users.noreply.github.com>
2024-05-29 17:25:39 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// JavaScript
+//console.log('Hello world!');
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    //direction: 'vertical',
+    loop: true,
+    autoplay:{
+        delay: 2500,
+        disableOnInteraction: false
+    },
+    
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
